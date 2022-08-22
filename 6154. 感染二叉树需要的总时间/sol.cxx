28868d0ec70b9aba5af6cf65d4f21d43c0b4958e
feiqi3 <zjgtzn@outlook.com>
2022-08-22 17:23:50 +0800

    Add sol to 6154.

--- 6154. 感染二叉树需要的总时间/sol.cxx
+++ 6154. 感染二叉树需要的总时间/sol.cxx
@@ -1,43 +1,44 @@
+#include <algorithm>
+#include <map>
 #define LEETCODE
 #include "../header.h"
 
-/*
-TODO
-TODO
-TODO
-TODO
-*/
-
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+   struct nn {
+    TreeNode *par;
+    bool flag = false;
+  };
 class Solution {
 public:
+  TreeNode *startNode;
+  map<int, nn> parM;
+  int _start;
+  void dfs(TreeNode *n, TreeNode *par) {
+    if (n == nullptr)
+      return;
+    //找到开始节点
+    if (n->val == _start)
+      startNode = n;
+    //保存该节点的父元素
+    parM[n->val] = {par, false};
+    dfs(n->left, n);
+    dfs(n->right, n);
+  }
   int getDepth(TreeNode *node) {
     if (node == nullptr)
       return 0;
-    return max(getDepth(node->left) + 1, getDepth(node->right) + 1);
-  }
-  int getFirstDepth(TreeNode *node, int &first) {
-    if (node == nullptr)
+    auto &n = parM[node->val];
+    if (n.flag == true)
       return 0;
-    if(node->val == first){
-        return 1;
-    }
-    int m = max(getDepth(node->left), getDepth(node->right));
-    return m > 0 ? m + 1 : 0;
+    n.flag = true;
+    //递归获取父元素/左右孩子的深度的最大值
+    return max(getDepth(node->left) + 1,
+               max(getDepth(node->right) + 1, getDepth(n.par) + 1));
   }
   int amountOfTime(TreeNode *root, int start) {
-
+    _start = start;
+    dfs(root, nullptr);
+    parM[start].flag = false;
+    //计算从初始节点到末端节点的最大深度
+    return getDepth(startNode) - 1;
   }
 };
