42b4bc43e4b22def66a46271f66af2c800943302
feiqi3 <zjgtzn@outlook.com>
2022-08-25 14:53:11 +0800

    TODO: 33

---
+++ 33. 搜索旋转排序数组/sol.cxx
@@ -0,0 +1,14 @@
+#include "../header.h"
+#include <vector>
+
+class Solution {
+public:
+  int search(vector<int> &nums, int target) {
+//TODO
+  }
+};
+int main() {
+  std::vector<int> m{5,1,2,3,4};
+  Solution s;
+  s.search(m, 1);
+}
