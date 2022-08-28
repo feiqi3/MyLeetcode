c95c7170cf8ac50840b565e8c1740a6ef111a8fc
feiqi3 <zjgtzn@outlook.com>
2022-08-28 12:58:12 +0800

    Add sol to 6160.

---
+++ 6160. 和有限的最长子序列/sol.cxx
@@ -0,0 +1,32 @@
+#include "../header.h"
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  vector<int> answerQueries(vector<int> &nums, vector<int> &queries) {
+    sort(nums.begin(), nums.end());
+    vector<int> ans;
+    for (int i = 0; i < queries.size(); i++) {
+      int s = queries[i];
+      int sum = 0;
+      int step = 0;
+      for (int j = 0; j < nums.size();j++) {
+        int tmp = sum+nums[j];
+        if(tmp > s){
+            break;
+        }
+        sum = tmp;
+        step++;
+      }
+      ans.push_back(step);
+    }
+    return ans;
+  }
+};
+int main() {
+  vector<int> a{469781,45635,628818,324948,343772,713803,452081};
+  vector<int> q{816646,929491};
+  Solution s;
+  s.answerQueries(a, q);
+}
