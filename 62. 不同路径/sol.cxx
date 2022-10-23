2bad75de659d31f1d06f8bab144ef1f69b384e24
feiqi3 <zjgtzn@outlook.com>
2022-10-23 14:43:20 +0800

    Bug fixes.

--- 62. 不同路径/sol.cxx
+++ 62. 不同路径/sol.cxx
@@ -4,7 +4,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        std::vector<vector<int>> DP(3,std::vector<int>(7,1));
+        std::vector<vector<int>> DP(m,std::vector<int>(n,1));
         for (int i = 1; i < m; i++) {
             for (int j = 1; j <n ; j++) {
                 DP[i][j] = DP[i-1][j] + DP[i][j-1];
